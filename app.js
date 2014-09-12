1455965bb63b49a2222ec31027036c3b62e79cec
kxy <kyrwastaken@gmail.com>
2014-09-12 23:52:47 +0200

    boosh

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
 
+var config = require('./app/config.json');
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/app/views')
+app.set('view engine', 'jade');
+
 app.get('/', function (req, res) {
-  res.sendfile('index.html');
+  res.render('load', { title: config.general.appName });
 });
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 1337);
