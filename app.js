65fcda0eefd83de8cdf48c20a22ebc60139c7a5c
kxy <kyrwastaken@gmail.com>
2014-09-13 22:46:28 +0200

    added file descriptions

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+// ---
+// ncht: app.js
+// created by @kxy
+// Kickstarts the application.
+// ---
+
 var fs = require('fs');
 var conf = require('nconf');
 
 //Load the Config file into nconf
 conf.file({ file: './app/config.json' });
 
+//Checks whether it's the first time running and opens installer
 if (conf.get('system:firstRun') === true) {
   require('./app/install/index').install(conf, function () {
     conf.set('system:firstRun', false);
--- app/install/index.js
+++ app/install/index.js
@@ -1,3 +1,9 @@
+// ---
+// ncht: app/install/index.js
+// created by @kxy
+// Main installer file.
+// ---
+
 var readline = require('readline');
 var rl = readline.createInterface({
   input: process.stdin,
@@ -7,7 +13,7 @@ var rl = readline.createInterface({
 
 module.exports = {
   install: function (conf, callback) {
-    console.log("ncht v0.0.1 installer \n~by @kxy & @pixeldesu~");
+    console.log("ncht v0.0.2 installer \n~by @kxy & @pixeldesu~");
     rl.question('What do you want to name your app? ', function (answer) {
       conf.set('general:appName', answer);
       console.log("Set App Name as " + answer);
