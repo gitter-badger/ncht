e63cf277578aeb8e42f985464c1ee7ff4e9d09c2
kxy <kyrwastaken@gmail.com>
2014-09-13 23:38:37 +0200

    ok

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-
-gulp.task('sass', function () {
-  gulp.src('./app/styles/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./app/public/css/style.css'));
-})
---
+++ readme.md
@@ -0,0 +1,1 @@
+#Development at develop branch.
